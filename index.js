3d11d232621599ff352836e9052105c6b9ac4075
BroFistMan123 <mumbukan_180000000423@uic.edu.ph>
2020-03-26 19:17:33 +0800

    added Port

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.get('/', (req, res) => {
 });
 
 // listen to requests
-app.listen(3000, () => {
-    console.log('app now listening for requests on port 3000');
-});
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log(`Server is running on ${PORT}`)
